d7bc1e676f98cde9a014cd4e3ebec0d5ab8993ec
ilturne <ilturne@siue.edu>
2024-04-19 17:38:56 -0500

    button test worked

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ from datetime import datetime
 import RPi.GPIO as GPIO
 
 # GPIO setup
-BUTTON_PIN = 17  # Change this based on your wiring
+BUTTON_PIN = 17  # Button wiring
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+def mode_switch():
+	if GPIO.input(BUTTON_PIN) == GPIO.LOW:
+		print("Button is pressed")
+	else:
+		print("Button is not pressed")
+	
 # LCD setup
 display = drivers.Lcd()
 
